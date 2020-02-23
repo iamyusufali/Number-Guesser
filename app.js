cee1b1393fe2aae6d613a7b4b9ffaa067237fe8b
Yusuf Ali <yali98319@gmail.com>
2020-02-23 20:45:26 +0530

    Added today's progress

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Document Elements
 const gameLevels = document.querySelector('.difficulty-levels'),
   easyMode = document.querySelector('button.easy'),
   moderateMode = document.querySelector('button.moderate'),
@@ -7,39 +8,56 @@ const gameLevels = document.querySelector('.difficulty-levels'),
   guessButton = document.querySelector('button.guess'),
   resultArea = document.querySelector('.result-area');
 
+// Load All Event listners
+loadAllEventListners();
+
 // Change Game Level
 function changeGameLevel(e) {
   if (e.target.classList.contains('level')) {
-    e.target.classList.add('active');
-    e.target.disabled = true;
-    guessInput.disabled = false;
-    guessInput.placeholder = 'Now type here';
+    // Show the level as Activated
+    activateLevel(e);
+
+    // Change the information based on level
+    changeLevelInfo(e);
+
+    // Generate a random number to check the result based on selected level
+    generateRandomNumber(e.target);
+
+    // Deactivate the level and make it enabled
     Array.from(gameLevels.children).forEach(element => {
       if (element.classList.contains('active') && element != e.target) {
         element.classList.remove('active');
         element.disabled = false;
       }
     });
-
-    // Change level info
-    if (e.target.classList.contains('easy')) {
-      levelInfo.textContent = `Guess a number between 1 and 10`;
-      levelInfo.style.color = '#4cb648';
-    } else if (e.target.classList.contains('moderate')) {
-      levelInfo.textContent = `Guess a number between 1 and 25`;
-      levelInfo.style.color = '#fcc72c';
-    } else if (e.target.classList.contains('hard')) {
-      levelInfo.textContent = `Guess a number between 1 and 50`;
-      levelInfo.style.color = '#f5744d';
-    }
-
-    // Generate random number
-    generateRandomNumber(e.target);
   }
 }
 
-// Generate random number based on selected level
-let randomNumber = 0;
+// Show the level as Activated
+function activateLevel(e) {
+  e.target.classList.add('active');
+  e.target.disabled = true;
+  guessInput.disabled = false;
+  guessButton.disabled = false;
+  guessInput.placeholder = 'Now type here';
+}
+
+// Change the information based on level
+function changeLevelInfo(e) {
+  if (e.target.classList.contains('easy')) {
+    levelInfo.textContent = `Guess a number between 1 and 10`;
+    levelInfo.style.color = '#4cb648';
+  } else if (e.target.classList.contains('moderate')) {
+    levelInfo.textContent = `Guess a number between 1 and 25`;
+    levelInfo.style.color = '#fcc72c';
+  } else if (e.target.classList.contains('hard')) {
+    levelInfo.textContent = `Guess a number between 1 and 50`;
+    levelInfo.style.color = '#f5744d';
+  }
+}
+
+// Generate a random number to check the result based on selected level
+let randomNumber = null;
 function generateRandomNumber(level) {
   if (level.classList.contains('easy')) {
     randomNumber = Math.floor(Math.random() * 10 + 1);
@@ -50,14 +68,27 @@ function generateRandomNumber(level) {
   }
 }
 
+// Remove border and change placeholder on input focus
+function removeBorderAndPlaceholder() {
+  guessInput.classList.remove('no-input');
+  guessInput.placeholder = 'Now type here';
+}
+
 // Check result
 function checkResult(e) {
-  if (parseFloat(guessInput.value) === randomNumber) {
-    console.log('Matched');
-    resultArea.style.display = 'block';
-    guessButton.textContent = 'Play Again';
+  if (guessInput.value === '') {
+    guessInput.classList.add('no-input');
+    guessInput.placeholder = 'Enter Guess?';
   } else {
-    attemptsLeft();
+    guessInput.classList.remove('no-input');
+
+    if (parseFloat(guessInput.value) === randomNumber) {
+      console.log('Matched');
+      resultArea.style.display = 'block';
+      guessButton.textContent = 'Play Again';
+    } else {
+      attemptsLeft();
+    }
   }
 }
 
@@ -72,6 +103,9 @@ function attemptsLeft() {
   resultArea.style.display = 'block';
 }
 
-// Event Listener
-gameLevels.addEventListener('click', changeGameLevel);
-guessButton.addEventListener('click', checkResult);
+// Event Listeners
+function loadAllEventListners() {
+  gameLevels.addEventListener('click', changeGameLevel);
+  guessButton.addEventListener('click', checkResult);
+  guessInput.addEventListener('focus', removeBorderAndPlaceholder);
+}
